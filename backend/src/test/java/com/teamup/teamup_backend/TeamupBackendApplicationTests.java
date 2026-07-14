345c1d5d908f42711f0c1853b3122ac50beef5ac
aadislnk <adityasohanslnk@gmail.com>
2026-07-14 12:46:11 +0530

    chore(project): initialize repository structure

---
+++ backend/src/test/java/com/teamup/teamup_backend/TeamupBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamup.teamup_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamupBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
