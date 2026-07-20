555d9a1007ab7513b5f6fed66807918c21139e1a
aadislnk <adityasohanslnk@gmail.com>
2026-07-20 20:48:36 +0530

    feat(auth): implement authentication module

--- backend/src/main/java/com/teamup/teamup_backend/config/OpenApiConfig.java
+++ backend/src/main/java/com/teamup/teamup_backend/config/OpenApiConfig.java
@@ -1,12 +1,23 @@
 package com.teamup.teamup_backend.config;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADERgit
+)
 public class OpenApiConfig {
 
     @Bean
@@ -19,11 +30,15 @@ public class OpenApiConfig {
 
         Info info = new Info()
                 .title("TeamUp API")
-                .description("TeamUp is a platform where students can discover hackathons and technical events, create or join teams, recruit members, and collaborate.")
                 .version("1.0.0")
+                .description("""
+                        TeamUp is a platform where students can discover hackathons and technical events,
+                        create or join teams, recruit members, and collaborate.
+                        """)
                 .contact(contact);
 
         return new OpenAPI()
-                .info(info);
+                .info(info)
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
     }
 }
